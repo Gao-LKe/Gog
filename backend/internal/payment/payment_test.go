0df177e1f501d7efea30c6d1318e274c79ae1b3a
gao66666 <64669137+gao66666@users.noreply.github.com>
2026-09-22 15:01:49 +0800

    test(payment): use in-memory sqlite database

--- backend/internal/payment/payment_test.go
+++ backend/internal/payment/payment_test.go
@@ -69,7 +69,7 @@ func TestIdempotencyKeyCannotBeReusedForAnotherOrder(t *testing.T) {
 
 func paymentTestDB(t *testing.T) *gorm.DB {
 	t.Helper()
-	db, err := gorm.Open(sqlite.Open(t.Name()+"?mode=memory&cache=shared"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("file:"+t.Name()+"?mode=memory&cache=shared"), &gorm.Config{})
 	if err != nil {
 		t.Fatal(err)
 	}
